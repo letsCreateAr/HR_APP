d18eac2280e0f39b776d2256bd4336ab4856e95c
letsCreateAr <69702665+letsCreateAr@users.noreply.github.com>
2023-06-23 16:19:53 -0700

    Add files via upload

---
+++ src/components/SideBar copy 3.jsx
@@ -0,0 +1,85 @@
+import { InsertEmoticon } from "@mui/icons-material";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+function SideBar(){
+    const [open, setOpen] = useState(true);
+    const Menus = [
+        { title: "Dashboard", path: "/", src: "Chart_fill" },
+        { title: "My Tasks", path: "/tasks", src: "Chat" },
+        { title: "Lunchtime", path: "/lunchtime", src: "User", gap: true },
+        { title: "Contact List", path: "/contactlist", src: "Calendar" },
+        { title: "My Info", path: "/myinfo", src: "Folder", gap: true },
+        { title: "Settings", path: "/settings", src: "Setting" },
+    ];
+
+    return (
+        <div className="flex">
+            <div
+                className={` ${
+                open ? "w-72" : "w-20 "
+                } bg-dark-purple h-screen p-5 pt-8 relative duration-300`}
+            >
+                <img
+                src="./src/assets/control.png"
+                className={`absolute cursor-pointer -right-3 top-9 w-7 border-dark-purple
+                border-2 rounded-full  ${!open && "rotate-180"}`}
+                onClick={() => setOpen(!open)}
+                />
+                <div className="flex flex-row gap-x-6 items-center">
+                    <div
+                        class="relative inline-flex items-center justify-center w-9 h-9 overflow-hidden rounded-full bg-gray-100"
+                        className={`cursor-pointer duration-500 ${
+                        open && "relative inline-flex items-center justify-center w-9 h-9 overflow-hidden rounded-full bg-gray-100"
+                        }`}
+                    >
+                    <div
+                        class="relative inline-flex items-center justify-center w-9 h-9 overflow-hidden rounded-full bg-gray-100"
+                        className={`cursor-pointer duration-500 ${
+                        !open && "rotate-[360deg] relative inline-flex items-center justify-center w-9 h-9 overflow-hidden rounded-full bg-gray-100"
+                        }`}
+                    >
+                        <span class="font-medium text-gray-600">JS</span>
+                    </div>
+                    </div>
+
+                    <div
+                        className={`relative flex-col space-y-0 duration-500 ${
+                        !open && "scale-0"
+                        }`}
+                    >
+                        <div className="whitespace-nowrap text-white origin-left font-medium text-xl">Jin Seok</div>
+                        <div className="whitespace-nowrap text-white origin-left font-regular text-xs">IT/HR</div>
+                    </div>
+                </div>
+
+                <ul className="pt-6">
+                    {Menus.map((Menu, index) => (
+                        <li
+                            key={index}
+                            className={`flex rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4 
+                            ${Menu.gap ? "mt-9" : "mt-2"} ${
+                                index === 0 && "bg-light-white"
+                            } `}
+                        >
+                            <Link to={Menu.path}>
+                            <div className="flex items-center">
+                            <img src={`./src/assets/${Menu.src}.png`} />
+                            <span className={`${!open && "hidden"} ml-4 whitespace-nowrap origin-left duration-200`}>
+                                {Menu.title}
+                            </span>
+                            </div>
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div className="h-screen flex-1 p-7">
+                <h1 className="text-2xl font-semibold ">Today's Announcements</h1>
+
+                <h1 className="text-2xl font-semibold ">My Tasks</h1>
+            </div>
+        </div>
+    );
+};
+export default SideBar;
